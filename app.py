adcd6c752ff619f0c2e11025a21124a2dbca45be
Pranav <pd2772001@gmail.com>
2024-01-01 11:23:52 +0530

    Added waitress service

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 
 from flask import Flask, render_template, request
 from my_app import get_competition_message  # Import the function from my_python_app
+from waitress import serve
 
 app = Flask(__name__)
 
@@ -17,4 +18,4 @@ def result():
     return render_template('result.html', message=compWhatsAppMessage)
 
 if __name__ == '__main__':
-    app.run(debug=False,host='0.0.0.0')
+    serve(app,host='0.0.0.0',threads=1)
